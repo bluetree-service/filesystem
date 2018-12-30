9f558e1dc23c4fcb74c0b7bd0227edc216991015
chajr <chajr@bluetree.pl>
2018-12-30 14:15:34 +0100

    Added some new events for copy

--- src/StaticObjects/FsInterface.php
+++ src/StaticObjects/FsInterface.php
@@ -14,4 +14,10 @@ interface FsInterface
     public const DELETE_PATHS_BEFORE = 'delete_paths_before';
     public const DELETE_PATH_CONTENT_AFTER = 'delete_path_content_after';
     public const DELETE_PATH_CONTENT_EXCEPTION = 'delete_path_content_exception';
+
+    public const COPY_PATH_CONTENT_EXCEPTION = 'copy_path_content_exception';
+    public const COPY_PATH_CONTENT_AFTER = 'copy_path_content_after';
+    public const COPY_PATH_CONTENT_BEFORE = 'copy_path_content_before';
+    public const COPY_PATHS_BEFORE = 'copy_paths_before';
+    public const COPY_CREATE_PATH_EXCEPTION = 'copy_create_path_exception';
 }
