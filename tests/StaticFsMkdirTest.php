f8a3ecde25fc641f277f06a5fa723f0251446292
chajr <chajr@bluetree.pl>
2019-01-19 10:31:27 +0100

    Added event tests form Fs::Mkdir

--- tests/StaticFsMkdirTest.php
+++ tests/StaticFsMkdirTest.php
@@ -3,7 +3,11 @@
 namespace BlueFilesystemTest;
 
 use PHPUnit\Framework\TestCase;
-use BlueFilesystem\StaticObjects\Fs;
+use BlueFilesystem\StaticObjects\{
+    Fs,
+    FsInterface
+};
+use BlueEvent\Event\Base\EventDispatcher;
 
 class StaticFsMkdirTest extends TestCase
 {
@@ -57,6 +61,51 @@ class StaticFsMkdirTest extends TestCase
         $out1 = Fs::mkdir(__DIR__ . '/playground/new_dir:?');
 
         $this->assertEmpty($out1);
+    }
+
+    public function testMkdirWithEvents(): void
+    {
+        $exceptionsExecutions = 0;
+        $afterExecutions = 0;
+        $beforeExecutions = 0;
+
+        $eventDispatcher = new EventDispatcher;
+        $eventDispatcher->setEventConfiguration([
+            FsInterface::CREATE_PATH_EXCEPTION => [
+                'object' => 'BlueEvent\Event\BaseEvent',
+                'listeners' => [
+                    function () use (&$exceptionsExecutions) {
+                        $exceptionsExecutions++;
+                    },
+                ],
+            ],
+            FsInterface::CREATE_PATH_AFTER => [
+                'object' => 'BlueEvent\Event\BaseEvent',
+                'listeners' => [
+                    function () use (&$afterExecutions) {
+                        $afterExecutions++;
+                    },
+                ],
+            ],
+            FsInterface::CREATE_PATH_BEFORE => [
+                'object' => 'BlueEvent\Event\BaseEvent',
+                'listeners' => [
+                    function () use (&$beforeExecutions) {
+                        $beforeExecutions++;
+                    },
+                ],
+            ],
+        ]);
+
+        Fs::configureEventHandler($eventDispatcher);
+
+        $this->testCreateDirectory();
+        $this->tearDown();
+        $this->testCreateDirWithError();
+
+        $this->assertEquals(1, $exceptionsExecutions);
+        $this->assertEquals(2, $afterExecutions);
+        $this->assertEquals(3, $beforeExecutions);
     }
 
     public function tearDown(): void
