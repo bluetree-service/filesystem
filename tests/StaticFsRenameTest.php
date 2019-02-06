916442dcdc0eda18e722720665ed18d0147eae3a
chajr <chajr@bluetree.pl>
2019-02-06 16:19:31 +0100

    Added error test for Fs::rename

--- tests/StaticFsRenameTest.php
+++ tests/StaticFsRenameTest.php
@@ -14,6 +14,7 @@ class StaticFsRenameTest extends TestCase
     public function setUp(): void
     {
         \shell_exec('chmod 0777 -R ' . StaticFsDelTest::TEST_DIR . ' > /dev/null 2>&1');
+        \shell_exec('rm -r ' . StaticFsDelTest::TEST_DIR . 'test > /dev/null 2>&1');
         \shell_exec('rm -r ' . StaticFsDelTest::TEST_DIR . 'file > /dev/null 2>&1');
         \shell_exec('rm -r ' . StaticFsDelTest::TEST_DIR . 'file2 > /dev/null 2>&1');
         \shell_exec('rm -r ' . StaticFsDelTest::TEST_DIR . '1 > /dev/null 2>&1');
@@ -48,9 +49,22 @@ class StaticFsRenameTest extends TestCase
         $this->assertFileNotExists(StaticFsDelTest::TEST_DIR . '1');
     }
 
-    public function testRenameWithException()
+    public function testRenameWithException(): void
     {
-        
+        $data = Fs::mkdir(StaticFsDelTest::TEST_DIR . 'test');
+        $this->assertTrue(Fs::validateComplexOutput($data));
+        $data = Fs::copy(__DIR__ . '/test-dirs/del/file', StaticFsDelTest::TEST_DIR . 'test/file');
+        $this->assertTrue(Fs::validateComplexOutput($data));
+
+        $this->assertFileNotExists(StaticFsDelTest::TEST_DIR . 'test/file2');
+
+        \shell_exec('chmod 0555 ' . StaticFsDelTest::TEST_DIR . 'test > /dev/null 2>&1');
+
+        $out = Fs::rename(StaticFsDelTest::TEST_DIR . 'test/file', StaticFsDelTest::TEST_DIR . 'test/file2');
+
+        $this->assertFalse($out);
+        $this->assertFileExists(StaticFsDelTest::TEST_DIR . 'file');
+        $this->assertFileNotExists(StaticFsDelTest::TEST_DIR . 'file2');
     }
 
     public function testRenameWithEvents()
